e4b79d8cfae555f8809471b1ae3c7fdd436e0e7e
Andrew Gait <andrew.gait@manchester.ac.uk>
2024-10-17 09:14:39 +0100

    Address majority of review comments:
    - move csv files into csv / test_data directories
    - edit scripts accordingly to match this
    - delete unneeded files

--- MPhys/Single_Emitter/concatenate_pandas_photon_dataframes.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-photon_detected = pd.read_csv('test_new_photons_detected_spectral.csv')
-
-# There were just under 10^6 elements in this dataset, so to get to 10^8, concatenate 110 of these together...
-
-photon_detected_large = pd.concat([photon_detected for _ in range(110)])
-
-photon_detected_large.to_csv('test_new_photons_detected_spectral_100000000.csv')
--- MPhys/Single_Emitter/mitsuba_hello.py
+++
@@ -1,7 +0,0 @@
-import mitsuba as mi
-
-mi.set_variant('scalar_rgb')
-
-img = mi.render(mi.load_dict(mi.cornell_box()))
-
-mi.Bitmap(img).write('cbox.exr')
