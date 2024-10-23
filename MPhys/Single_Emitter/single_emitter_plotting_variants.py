f4d5a1771c0cbea6d55e23be1e5eba7226e03270
Andrew Gait <andrew.gait@manchester.ac.uk>
2024-10-23 01:20:30 +0100

    Add error box-plots to plots

--- MPhys/Single_Emitter/single_emitter_plotting_variants.py
+++ MPhys/Single_Emitter/single_emitter_plotting_variants.py
@@ -9,24 +9,48 @@ variants = ["llvm_mono", "llvm_ad_rgb"]  # , "cuda_mono", "cuda_ad_rgb"]
 
 max_time = 0
 timing_data = []
+timing_full_data = []
 for variant in variants:
+    full_data = np.loadtxt("csv/"+variant+"_full_timing_for_n_photons.csv", delimiter=",")
     data = np.loadtxt("csv/"+variant+"_stats_timing_for_n_photons.csv", delimiter=",")
     max_full = np.max([d[7] for d in data])
     if max_full > max_time:
         max_time = max_full
     timing_data.append([variant, data])
+    timing_full_data.append(full_data)
 
 print(timing_data)
 
 # Plot each variant for each column 
 columns = ["load", "render", "load_and_render", "full_time"]
 
-for ncol, column in enumerate(columns):
+for n_col, column in enumerate(columns):
     fig = plt.figure()
     for n_var, variant in enumerate(variants):
-        plt.plot([tn[0] for tn in timing_data[n_var][1]], [tn[2*ncol+1] for tn in timing_data[n_var][1]], label=column+' '+timing_data[n_var][0])
+        n_photons_list = [tn[0] for tn in timing_data[n_var][1]]
+        plt.plot(n_photons_list, [tn[2*n_col+1] for tn in timing_data[n_var][1]], label=column+' '+timing_data[n_var][0])
+        # boxplot, somehow - get the relevant column from the full data
+        full_variant_data = timing_full_data[n_var]
+        timing_vs_nphotons_dict = {}
+
+        for t_data in full_variant_data:
+            [n_photons, load_time, render_time, load_and_render_time, elapsed_time] = t_data
+            timing_vs_nphotons_dict[n_photons] = []
+
+        for t_data in full_variant_data:
+            [n_photons, load_time, render_time, load_and_render_time, elapsed_time] = t_data
+            timing_vs_nphotons_dict[n_photons].append([load_time, render_time, load_and_render_time, elapsed_time])
+
+        data = []
+        for key in timing_vs_nphotons_dict.keys():
+            data.append([f[n_col] for f in timing_vs_nphotons_dict[key]])
+        fixed_w = 0.1
+        width = lambda p, w: 10**(np.log10(p)+w/2.)-10**(np.log10(p)-w/2.)
+        plt.boxplot(data, positions=n_photons_list, widths=width(n_photons_list, fixed_w))
+
     plt.legend()
-    plt.ylim(1e-3, max_time+(0.1*max_time))
+    plt.xlim(1e-1, 1e9)
+    plt.ylim(1e-3, max_time+(0.5*max_time))
     plt.xscale('log')
     plt.yscale('log')
     plt.ylabel('time (s)')
